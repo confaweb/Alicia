1cfc9d097e17e942c0c2c9a73763c3749ff89d58
Confalonieri Borojovich Damian Lionel <103534724+confaweb@users.noreply.github.com>
2023-12-13 22:33:38 -0300

    Project Init

---
+++ src/ar/edu/unlam/alicia/testing/AliciaTestCases.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.alicia.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AliciaTestCases {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Alicia {
+	requires junit;
+}
